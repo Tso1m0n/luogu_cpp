a0666fef8d801f2dfa9d82eccef23ec88306b4d5
Tso1m0n <cpppython@163.com>
2026-02-03 14:16:48 +0800

    test new repos

---
+++ 101/P5711【深基3.例3】闰年判断.cpp
@@ -0,0 +1,16 @@
+/*
+ *	@brief      https://www.luogu.com.cn/problem/P1001
+ *	@date       20260203
+*/
+
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+        std::cout << '1';
+    else
+        std::cout << '0';
+}
